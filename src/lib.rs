b33e271455247d5edb15a568a07f0e9d3189a63d
Jonathan Giusino <jgiusino@gmail.com>
2020-02-26 23:17:16 -0500

    added dat.gui and dimension/gravity control

--- src/lib.rs
+++ src/lib.rs
@@ -31,14 +31,49 @@ pub struct Particle {
 }
 
 impl Particle {
-    fn tick(&mut self, dt: f32) {
+    fn tick(&mut self, dt: f32, g: f32) {
         // apply gravitational force
-        self.velocity.y -= 9.81 * dt;
+        self.velocity.y -= g * dt;
 
         // update positions based on velocity
         self.position.x += self.velocity.x * dt;
         self.position.y += self.velocity.y * dt;
         self.position.z += self.velocity.z * dt;
+    }
+
+    fn new() -> Particle {
+        let position = Vector3D { 
+            x: (Math::random() as f32)*1000.0,
+            y: (Math::random() as f32)*1000.0,
+            z: (Math::random() as f32)*1000.0,
+        };
+        let velocity = Vector3D { 
+            x: (Math::random() as f32)*10.0,
+            y: (Math::random() as f32)*10.0,
+            z: (Math::random() as f32)*10.0,
+        };
+        
+        Particle {
+            position,
+            velocity,
+        }
+    }
+}
+
+pub struct Config {
+    particle_num: usize,
+    gravity: f32,
+}
+
+impl Config {
+    pub fn new() -> Config {
+        let particle_num = 500;
+        let gravity = 9.81;
+
+        Config {  
+            particle_num,
+            gravity,
+        }
     }
 }
 
@@ -47,12 +82,14 @@ pub struct Simulation {
     origin: Vector3D,
     edge: Vector3D,
     particles: Vec<Particle>,
+    config: Config,
 }
 
 #[wasm_bindgen]
 impl Simulation {
     
     pub fn new() -> Simulation {
+        let config = Config::new();
         let origin = Vector3D {
             x: 0.0,
             y: 0.0,
@@ -64,36 +101,23 @@ impl Simulation {
             z: 100.0,
         };
 
-        let mut particles : Vec<Particle> = Vec::with_capacity(2000);
+        let mut particles: Vec<Particle> = Vec::with_capacity(config.particle_num);
         for _ in 0..particles.capacity() {
-            let position = Vector3D { 
-                x: (Math::random() as f32)*100.0,
-                y: (Math::random() as f32)*100.0,
-                z: (Math::random() as f32)*100.0,
-            };
-            let velocity = Vector3D { 
-                x: (Math::random() as f32)*10.0,
-                y: (Math::random() as f32)*10.0,
-                z: (Math::random() as f32)*10.0,
-            };
-            let p = Particle {
-                position,
-                velocity,
-            };
-            particles.push(p);
+            particles.push(Particle::new());
         }
         
         Simulation {
             origin,
             edge,
             particles,
+            config,
         }
     }
 
     pub fn tick(&mut self, dt: f32) {
         for i in 0..self.particles.len() {
             let p = &mut self.particles[i];
-            p.tick(dt);
+            p.tick(dt, self.config.gravity);
         }
     }
     
@@ -128,11 +152,46 @@ impl Simulation {
         }
     }
 
+    pub fn height(&self) -> f32 {
+        self.edge.y
+    }
+    pub fn width(&self) -> f32 {
+        self.edge.x
+    }
+    pub fn depth(&self) -> f32 {
+        self.edge.z
+    }
+
+    pub fn update_height(&mut self, height: f32) {
+        self.edge.y = height;
+    }
+
+    pub fn update_width(&mut self, width: f32) {
+        self.edge.x = width;
+    }
+
+    pub fn update_depth(&mut self, depth: f32) {
+        self.edge.z = depth;
+    }
+
     pub fn particles(&self) -> *const Particle {
         self.particles.as_ptr()
     }
 
     pub fn particle_count(&self) -> u32 {
         self.particles.len() as u32
+    }
+
+    pub fn update_particles(&mut self, count: u32) {
+        self.particles.clear();
+        // finish updating particles
+    }
+
+    pub fn gravity(&self) -> f32 {
+        self.config.gravity
+    }
+
+    pub fn update_gravity(&mut self, g: f32) {
+        self.config.gravity = g;
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,18 +1,29 @@
 import * as wasm from "wasm-particle-physics";
 import { memory } from "wasm-particle-physics/wasm_particle_physics_bg"
 
-import * as THREE from "./js/three.module.js";
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import * as dat from 'dat.gui';
 
-const simulation = wasm.Simulation.new();
-const particleCount = simulation.particle_count();
+
+
+var simulation = wasm.Simulation.new();
+var particleCount = simulation.particle_count();
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+);
+
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+var controls = new OrbitControls(camera, renderer.domElement);
 var geometry = new THREE.SphereGeometry(0.25,8,8);
 var material = new THREE.MeshNormalMaterial();
 
@@ -25,15 +36,28 @@ for (let index = 0; index < particleCount; index++) {
 
 meshArray.forEach( mesh => scene.add( mesh ));
 
-camera.position.z = 100;
-camera.position.x = 50;
-camera.position.y = 1;
+camera.position.set( 0, 20, 100);
 
 var particlesPtr = simulation.particles();
 var particles = new Float32Array(memory.buffer, particlesPtr, particleCount*6);
 
+class Options {
+    constructor() {
+        this.height = simulation.height();
+        this.width = simulation.width();
+        this.depth = simulation.depth();
+        this.gravity = simulation.gravity();
+    }
+}
+
 function animate() {
     requestAnimationFrame( animate );
+    simulationUpdate();
+    controls.update();
+    renderer.render( scene, camera );
+}
+
+function simulationUpdate() {
     simulation.tick(0.1);
     simulation.check_collision();
     for (let index = 0; index < meshArray.length; index++) {
@@ -45,7 +69,28 @@ function animate() {
             particles[j + 2],
         );
     }
-    renderer.render( scene, camera );
 }
 
+function addGUI() {
+    var gui = new dat.GUI();
+    var text = new Options;
+    var height = gui.add(text, 'height', 0.0, 1000.0);
+    var width = gui.add(text, 'width', 0.0, 1000.0);
+    var depth = gui.add(text, 'depth', 0.0, 1000.0);
+    var gravity = gui.add(text, 'gravity', 0.0, 100.0);
+    height.onChange((value) => {
+        simulation.update_height(value);
+    });
+    width.onChange((value) => {
+        simulation.update_width(value);
+    });
+    depth.onChange((value) => {
+        simulation.update_depth(value);
+    });
+    gravity.onChange((value) => {
+        simulation.update_gravity(value);
+    });
+}
+
+addGUI();
 animate();
